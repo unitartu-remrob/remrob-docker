241a2294c519c3ce161fd273dd01a4e2ccb9a317
Tsapu <chooky823@gmail.com>
2024-02-28 19:57:03 +0200

    rviz metrics automated test script

--- scripts/performance_test.sh
+++ scripts/performance_test.sh
@@ -12,25 +12,47 @@ run_in_new_terminal() {
     "
 }
 
-# run_in_new_terminal "roslaunch robotont_driver fake_driver.launch"
+run_in_new_tab() {
+    gnome-terminal --tab -- bash -c "
+        cd \$HOME &&
+        . /etc/environment &&
+        export VGL_DISPLAY=:1;
+        source /opt/ros/noetic/setup.bash &&
+        source $HOME/catkin_ws/devel/setup.bash &&
+        vglrun $1;
+    "
+}
+
+
 # run_in_new_terminal "rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
 
-run_in_new_terminal "roslaunch robotont_gazebo world_minimaze.launch"
-sleep 0.2;
+run_in_new_terminal "roslaunch robotont_driver fake_driver.launch"
+# run_in_new_terminal "roslaunch robotont_gazebo world_minimaze.launch"
+sleep 0.5;
 
-run_in_new_terminal "rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
-sleep 6;
-xdotool keydown q;
-sleep 0.7;
+run_in_new_tab "rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
+sleep 2;
 
 teleop_terminal=$(xdotool search --name "Terminal" | tail -1);
 xdotool windowsize $teleop_terminal 150 100 windowmove $teleop_terminal 10 10
+xdotool windowraise $teleop_terminal
+xdotool windowactivate $teleop_terminal
+
+# teleop_terminal=$(xdotool search --name "kasutaja" | head -n 1);
+# xdotool windowsize $teleop_terminal 150 100 windowmove $teleop_terminal 10 10
+# xdotool windowraise $teleop_terminal
+# xdotool windowactivate $teleop_terminal
+
+sleep 1;
+xdotool keydown q;
+sleep 0.6;
 
 xdotool keydown j;
-sleep 15;
+sleep 14;
 
 xdotool keyup j;
-xdo close -a Gazebo
+xdo close -dr
+# xdo close -a Gazebo
 
 
 # window_id=$(xdotool search --name "^Gazebo$" | head -n 1);
